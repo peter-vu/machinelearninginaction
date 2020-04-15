9e491e5a6c63b3b033310e3e742f9e63be27b097
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2020-04-15 22:44:41 +0800

    chapter 4

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -124,13 +124,13 @@ def calcMostFreq(vocabList, fullText):
 def localWords(feed1, feed0):
     import feedparser
     docList = []; classList = []; fullText = []
-    minLen = min(len(feed1['entries'],len(feed2['entries'])))
+    minLen = min(len(feed1['entries']),len(feed0['entries']))
     for i in range(minLen):
-        wordList = textParse(feed1['entries'][i]['summary'])
+        wordList = textParse(feed1['entries'][i]['title'])
         docList.append(wordList)
         fullText.append(wordList)
         classList.append(1)
-        wordList = textParse(feed2['entries'][i]['summary'])
+        wordList = textParse(feed0['entries'][i]['title'])
         docList.append(wordList)
         fullText.append(wordList)
         classList.append(0)
@@ -142,11 +142,11 @@ def localWords(feed1, feed0):
     for i in range(20):
         randIndex = int(random.uniform(0, len(trainingSet)))
         testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])
-    trainMat = []; trainingClasses = []
+        del(list(trainingSet)[randIndex])
+    trainMat = []; trainClasses = []
     for docIndex in trainingSet:
         trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
-        trainingClasses.append(classList[docIndex])
+        trainClasses.append(classList[docIndex])
     p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
     errorCount = 0
     for docIndex in testSet:
@@ -156,10 +156,21 @@ def localWords(feed1, feed0):
     print ('the error rate is: ', float(errorCount)/len(testSet))
     return vocabList, p0V, p1V
         
-    
-    
-    
-    
+def getTopWords(ny, sf):
+    import operator
+    vocabList, p0V, p1V = localWords(ny, sf)
+    topNY=[];topSF=[]
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0 : topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] > -6.0 : topNY.append((vocabList[i], p1V[i]))
+    sortedSF = sorted(topSF, key= lambda pair: pair[1], reverse=True)
+    print ("SF****")
+    for item in sortedSF:
+        print (item[0])
+    sortedNY = sorted(topNY, key= lambda pair: pair[1], reverse=True)
+    print ("NY***")
+    for item in sortedNY:
+        print (item[0])
     
     
     
