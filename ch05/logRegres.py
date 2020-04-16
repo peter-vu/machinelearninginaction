e86f344a97effd11913fed18fe202b76801b06bf
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2020-04-16 22:11:28 +0800

    ch5

--- ch05/logRegres.py
+++ ch05/logRegres.py
@@ -54,3 +54,64 @@ def plotBestFit(weight):
     ax.plot(x,mat(y).transpose())
     plt.xlabel('X1');plt.ylabel('X2')
     plt.show()
+    
+def stocGradAscent0(dataMatrix, classLabels):
+    m, n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i]*weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
