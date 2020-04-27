d26eb41d07ae9e5b083a0d55730053e66ead72c1
Vu Tuan Anh <vutuananh.vn.sg@gmail.com>
2020-04-28 00:49:24 +0800

    add summary

---
+++ ch07/adaboost.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+
+
+def loadSimpData():
+    datMat = array([[1.0, 2.1],
+                     [2.0, 1.1],
+                     [1.3, 1.1],
+                     [1.0, 1.0],
+                     [2.0, 1.0]
+                     ])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+
+def stumpClassify(dataMatrix, dimen, threshVal, thresIneq):
+    retArray = ones((shape(dataMatrix)[0],1))
+    if thresIneq == 'lt':
+        retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:,dimen] > threshVal] = -1.0
+    return retArray
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = mat(dataArr); labelMat = mat(classLabels).T
+    m,n = shape(dataMatrix)
+    numSteps = 10.0; bestStump = {}; bestClasEst = mat(zeros((m,1)))
+    minError = inf
+    for i in range(n):
+        rangeMin = dataMatrix[:,i].min(); rangeMax = dataMatrix[:,i].max();
+        stepSize = (rangeMax - rangeMin)/numSteps
+        for j in range(-1, int(numSteps)+1):
+            for inequal in ['lt','gt']:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = mat(ones((m,1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T*errArr
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump ['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump, minError, bestClasEst
+
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)   #init D to all equal
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst = buildStump(dataArr,classLabels,D)#build Stump
+        #print "D:",D.T
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))#calc alpha, throw in max(error,eps) to account for error=0
+        bestStump['alpha'] = alpha  
+        weakClassArr.append(bestStump)                  #store Stump Params in Array
+        #print "classEst: ",classEst.T
+        expon = multiply(-1*alpha*mat(classLabels).T,classEst) #exponent for D calc, getting messy
+        D = multiply(D,exp(expon))                              #Calc New D for next iteration
+        D = D/D.sum()
+        #calc training error of all classifiers, if this is 0 quit for loop early (use break)
+        aggClassEst += alpha*classEst
+        #print "aggClassEst: ",aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T,ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print ("total error: ",errorRate)
+        if errorRate == 0.0: break
+    return weakClassArr
